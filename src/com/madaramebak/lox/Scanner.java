de7fbdd20d58ecc204a0dde2f6033d4fe0bfe3f1
madaramebak <zakariahas07@gmail.com>
2025-06-01 09:58:57 -0400

    Scanner draft complete

--- src/com/madaramebak/lox/Scanner.java
+++ src/com/madaramebak/lox/Scanner.java
@@ -1,9 +1,7 @@
 package com.madaramebak.lox;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.madaramebak.lox.TokenType.*;
+import java.util.*;
 
 class Scanner {
   private final String source;
@@ -11,6 +9,28 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
+  private static final Map<String, TokenType> keywords;
+
+  // Some standard keywords
+  static {
+   keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -60,14 +80,161 @@ class Scanner {
       case '*':
          addToken(STAR); 
          break; 
+         // Started using some ternary operators here
+      case '!':
+         addToken(match('=') ? BANG_EQUAL : BANG);
+         break;
+      case '=':
+         addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+         break;
+      case '<':
+         addToken(match('<') ? LESS_EQUAL : LESS);
+         break;
+      case '>' :
+         addToken(match('>') ? GREATER_EQUAL : GREATER);
+         break;
+      case '/':
+         if(match('/')){
+            //accounting for the fact that comments run until the end of a line
+            while(peek() != '\n' && !isAtEnd()){
+               advance();
+            }
+         }else{
+            addToken(SLASH);
+         }
+         break;
+      case ' ':
+      case '\r':
+      case '\t':
+         break;
+
+      case '\n' :
+         line++;
+         break;
+
+      case '"' :
+         String();
+         break;
+
       default:
+         if(isDigit(c)){
+            number();
+         }else if (isAlpha(c)) {
+             identifier();
+         }else{
         Lox.error(line, "Unexpected character.");
+         }
         break;
     }
   }
 
+  private void identifier(){
+   while (isAlphaNumeric(peek())){
+      advance();
+   }
+   String text = source.substring(start, current);
+   TokenType type = keywords.get(text);
+   if(type == null){
+      type = IDENTIFIER;
+   }
+   addToken(IDENTIFIER);
+  }
+
+  private void number(){
+   while(isDigit(peek())){
+      advance();
+   }
+   
+   if(peek() == '.' && isDigit(peekNext())){
+      //consuming the dot
+      advance();
+
+      while(isDigit(peek())){
+         advance();
+      }
+   }
+   addToken(NUMBER, Double.parseDouble(source.substring(start,current)));
+  }
+
+// Supports multi-line strings
+  private void String(){
+   while(peek() != '"' && !isAtEnd()){
+      if(peek() == '\n'){
+         line++;
+      }
+      advance();
+   }
+
+      if(isAtEnd()){
+         Lox.error(line, "Unterminated String");
+         return;
+      }
+
+      advance();
+
+      // Trim quotes
+      String value = source.substring(start + 1, current - 1);
+      addToken(STRING, value);
+   }
+  
+
+  private boolean match(char expected){
+   if(isAtEnd() || source.charAt(current) != expected){
+      return false;
+   }
+   current++;
+
+   return true;
+   }
+
+/*
+lookahead helper method, the lexical grammar of the lox language 
+dictates that we only need one character of lookahead
+*/
+   private char peek(){
+      if(isAtEnd()){
+         return '\0';
+      }
+      return source.charAt(current);
+   }
+
+   private char peekNext(){
+      if(current + 1 >= source.length()){
+         return '\0';
+      }
+
+      return source.charAt(current + 1);
+   }
+
+   private boolean isAlpha(char c){
+      return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+   }
+
+   private boolean isAlphaNumeric(char c){
+      return isAlpha(c) ||isDigit(c);
+   }
+
+// Comparing unicode values
+   private boolean isDigit(char c){
+      return c >= '0' && c <= '9';
+   }
+  
+
   
   private boolean isAtEnd() {
     return current >= source.length();
+  }
+
+  private char advance(){
+   return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type){
+   addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal){
+   String text = source.substring(start, current);
+   tokens.add(new Token(type, text, literal, line));
   }
 }
